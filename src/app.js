d5fd2d8d179fff4282537cb98a09b4ef9fa465ea
pakornsukmankong <floksong.001@gmail.com>
2022-10-14 16:09:19 +0700

    feat: error-middleware

--- src/app.js
+++ src/app.js
@@ -13,5 +13,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(notFound)
+app.use(error)
+
 const port = process.env.PORT || 8888
 app.listen(port, () => console.log(`server running at port ${port}\n\n\n\n\n`))

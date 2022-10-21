4df28f4ec616aaf17bd7c2398c3fda270ca79c8c
prachayaOrr <prachaya.orr@gmail.com>
2022-10-21 14:35:17 +0700

    fixed: Model property, feat: add some reserve-api

--- src/controller/reserveController.js
+++ src/controller/reserveController.js
@@ -1,5 +1,33 @@
+const { Reserve } = require('../models');
+
 exports.createReserve = async (req, res, next) => {
   try {
-  } catch (err) {}
-  next();
+    const {
+      propertyId, // send from frontEnd
+      pricePerDate,
+      guestsCount,
+      cleaningFees,
+      amountPaid,
+      serviceFees,
+      checkInDate,
+      checkOutDate,
+    } = req.body;
+
+    const data = {
+      userId: req.user.id, // req.user.id from authenticate
+      propertyId,
+      pricePerDate,
+      guestsCount,
+      cleaningFees,
+      amountPaid,
+      serviceFees,
+      checkInDate,
+      checkOutDate,
+    };
+
+    const newReserve = await Reserve.create(data);
+    res.status(201).json({ newReserve });
+  } catch (err) {
+    next(err);
+  }
 };

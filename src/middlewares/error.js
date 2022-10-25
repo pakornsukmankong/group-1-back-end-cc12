eabf050f0b30953bbe419efa707ecda343c64fb6
PobSRW <sorawat.vi@gmail.com>
2022-10-25 18:53:30 +0700

    update confirm password

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,16 +1,16 @@
 module.exports = (err, req, res, next) => {
-  console.log(err)
+	console.log(err.statusCode);
+	console.log('a rai suk yang nung');
+	if (
+		err.name === 'SequelizeValidationError' ||
+		err.name === 'SequelizeUniqueConstraintError'
+	) {
+		err.statusCode = 400;
+		err.message = err.errors[0].message;
+	}
 
-  if (
-    err.name === 'SequelizeValidationError' ||
-    err.name === 'SequelizeUniqueConstraintError'
-  ) {
-    err.statusCode = 400
-    err.message = err.errors[0].message
-  }
-
-  if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') {
-    err.statusCode = 401
-  }
-  res.status(err.statusCode || 500).json({ message: err.message })
-}
+	if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') {
+		err.statusCode = 401;
+	}
+	res.status(err.statusCode || 500).json({ message: err.message });
+};

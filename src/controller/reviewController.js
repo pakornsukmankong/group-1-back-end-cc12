2988de9f122d89557fbc4781b82b9497aff9ad69
prachayaOrr <prachaya.orr@gmail.com>
2022-10-28 13:49:00 +0700

    fix createReview

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -3,55 +3,60 @@ const AppError = require('../utils/appError');
 const { Op } = require('sequelize');
 
 exports.createReview = async (req, res, next) => {
-	try {
-		const { propertyId } = req.params;
-		const { comment } = req.body;
+  try {
+    const { propertyId } = req.params;
+    const { comment } = req.body;
 
-		if (!comment || !String(comment)) {
-			throw new AppError('comment is invalid', 400);
-		}
-		const book = await Booking.findOne({
-			where: { [Op.and]: [{ userId: req.user.id }, { propertyId }] },
-		});
+    if (!comment || !String(comment)) {
+      throw new AppError('comment is invalid', 400);
+    }
+    const book = await Booking.findOne({
+      where: { [Op.and]: [{ userId: req.user.id }, { propertyId }] },
+    });
 
-		if (!book) {
-			throw new AppError('you are not booking yet', 400);
-		}
+    if (!book) {
+      throw new AppError('you are not booking yet', 400);
+    }
 
-		const checkReview = await PropertyReview.findOne({
-			where: {
-				[Op.and]: [{ propertyId: propertyId }, { userId: req.user.id }],
-			},
-		});
+    const checkReview = await PropertyReview.findOne({
+      where: {
+        [Op.and]: [{ propertyId: propertyId }, { userId: req.user.id }],
+      },
+    });
 
-		if (checkReview) {
-			throw new AppError('you already reviewed', 400);
-		}
-		const review = await PropertyReview.create({
-			comment,
-			userId: req.user.id,
-			propertyId: propertyId,
-			bookingId: book.id,
-		});
+    if (checkReview) {
+      throw new AppError('you already reviewed', 400);
+    }
+    const review = await PropertyReview.create({
+      comment,
+      userId: req.user.id,
+      propertyId: propertyId,
+      bookingId: book.id,
+    });
 
-		res.status(201).json({ review });
-	} catch (err) {
-		next(err);
-	}
+    const newReview = await PropertyReview.findOne({
+      where: { id: review.id },
+      include: { model: User, attributes: { exclude: ['password'] } },
+    });
+
+    res.status(201).json({ newReview });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getAllReviewByPropertyId = async (req, res, next) => {
-	try {
-		const { propertyId } = req.params;
+  try {
+    const { propertyId } = req.params;
 
-		const review = await PropertyReview.findAll({
-			where: { propertyId },
-			// attributes: { exclude: ['userHostId', 'propertyId', 'bookingId'] },
-			include: { model: User, attributes: { exclude: ['password'] } },
-			order:[['createdAt', 'DESC']],
-		});
-		res.status(201).json({ review });
-	} catch (err) {
-		next(err);
-	}
+    const review = await PropertyReview.findAll({
+      where: { propertyId },
+      // attributes: { exclude: ['userHostId', 'propertyId', 'bookingId'] },
+      include: { model: User, attributes: { exclude: ['password'] } },
+      order: [['createdAt', 'DESC']],
+    });
+    res.status(201).json({ review });
+  } catch (err) {
+    next(err);
+  }
 };
